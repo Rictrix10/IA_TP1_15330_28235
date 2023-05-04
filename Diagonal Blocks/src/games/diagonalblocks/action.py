bdb3061cd937b256fdd9c7b2d7c6f4832632b27c
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-05-04 15:06:35 +0100

    As linhas e colunas estavam trocadas

--- Diagonal Blocks/src/games/diagonalblocks/action.py
+++ Diagonal Blocks/src/games/diagonalblocks/action.py
@@ -2,22 +2,23 @@ class DiagonalBlocksAction:
     """
     a connect 4 action is simple - it only takes the value of the column to play
     """
-    __col: int
     __row: int
+    __col: int
     __piece: int
     __option: int
 
-    def __init__(self, col: int, row: int, piece: int, option: int):
-        self.__col = col
+    def __init__(self, row: int, col: int,  piece: int, option: int):
         self.__row = row
+        self.__col = col
         self.__piece = piece
         self.__option = option
 
+    def get_row(self):
+        return self.__row
+    
     def get_col(self):
         return self.__col
 
-    def get_row(self):
-        return self.__row
 
     def get_piece(self):
         return self.__piece
